257d2de6008f63d1921d2d63d6d0eca89a91a916
tuenguyen <tuenguyen553@gmail.com>
2025-03-25 00:07:11 +0700

    minor

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -9,7 +9,7 @@ module.exports = {
       max_memory_restart: '1G',
       env: {
         NODE_ENV: 'development',
-        PORT: 3000,
+        PORT: 5123,
       },
       env_file: '.env',
     },
